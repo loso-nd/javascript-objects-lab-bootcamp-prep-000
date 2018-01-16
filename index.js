f04cd1e3493b19ddad2569a9d6fe726befbe3109
Nahomie <nducasse3@gmail.com>
2018-01-16 18:17:44 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,7 +1,3 @@
 function updateObjectWithKeyAndValue(object, key, value){
   return Object.assign(object, key, value)
 }
-
-function updateObjectWithKeyAndValue(object, key, value){
-  return Object.assign({}, object, key, value)
-}
