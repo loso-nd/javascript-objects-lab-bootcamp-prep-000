a9b4478e067aac47c31ded6ed7b214a71ec102d8
Nahomie <nducasse3@gmail.com>
2018-01-16 18:28:29 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -9,5 +9,5 @@ function updateObjectWithKeyAndValue(object, key, value){
 function destructivelyUpdateObjectWithKeyAndValue(object, key, value){
   object[key] = value
 
-  return obj
+  return Object.assign({});
 }
